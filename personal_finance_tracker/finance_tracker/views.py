787dc526561c940bfaf6d031b8831e1ad9bc8edc
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2025-01-20 20:55:37 +0100

    chore: link styles.css to index.html

--- personal_finance_tracker/finance_tracker/views.py
+++ personal_finance_tracker/finance_tracker/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "finance_tracker/index.html")
