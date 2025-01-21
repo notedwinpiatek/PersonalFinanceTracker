5f05ae273498d8cad79c3a38ebe81ece8e83c191
Julia Max <jjuliamaxxx@gmail.com>
2025-01-21 20:11:40 +0100

    style: register form

--- personal_finance_tracker/finance_tracker/static/finance_tracker/script.js
+++ personal_finance_tracker/finance_tracker/static/finance_tracker/script.js
@@ -1,2 +1,4 @@
-document.querySelector('input[name="username"]').setAttribute('placeholder', 'Username');
-document.querySelector('input[name="password"]').setAttribute('placeholder', 'Password');
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('input[name="username"]').setAttribute('placeholder', 'Username');
+    document.querySelector('input[name="password"]').setAttribute('placeholder', 'Password');
+});
