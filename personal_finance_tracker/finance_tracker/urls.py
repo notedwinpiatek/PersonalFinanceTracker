3c5583009a823df55fc175669595626727200e0d
Julia Max <jjuliamaxxx@gmail.com>
2025-01-21 17:35:28 +0100

    fix: no access to login and register if logged in

--- personal_finance_tracker/finance_tracker/urls.py
+++ personal_finance_tracker/finance_tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
