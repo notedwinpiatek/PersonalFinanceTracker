3f2b797af75cabb560f0a9e7843772f8676fbcd4
Julia Max <jjuliamaxxx@gmail.com>
2025-01-21 20:13:06 +0100

    refactor: move js related to login to login.html

--- personal_finance_tracker/finance_tracker/static/finance_tracker/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('input[name="username"]').setAttribute('placeholder', 'Username');
-    document.querySelector('input[name="password"]').setAttribute('placeholder', 'Password');
-});
