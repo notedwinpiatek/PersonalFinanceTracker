f7aa362c2b6a1e2a586472066d094a04deace3b2
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2025-01-22 22:20:27 +0100

    fix: make account and pswd change urls work

--- personal_finance_tracker/finance_tracker/urls.py
+++ personal_finance_tracker/finance_tracker/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('account/', views.account_settings, name='account_settings'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
@@ -10,8 +13,4 @@ urlpatterns = [
     path('expenses/<str:month_name>/', views.expenses, name='expenses'),
     path('<str:month_name>/', views.index, name='index'),
     path('', views.index, name='index'),
-    path('account/', views.account_settings, name='account_settings'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change/password_change.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
-    
 ]
