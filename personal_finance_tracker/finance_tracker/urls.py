13abf3f25a57a676ec7694e9736efd5f76e4b79a
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2025-01-23 16:20:39 +0100

    feat: user password change implementation

--- personal_finance_tracker/finance_tracker/urls.py
+++ personal_finance_tracker/finance_tracker/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('account/', views.account_settings, name='account_settings'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change/password_change.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+    path('password_change/', views.custom_password_change, name='password_change'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
