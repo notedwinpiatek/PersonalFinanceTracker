902ed73de3989ae89a3e2c63df87387a0032b6cd
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2025-01-23 23:47:40 +0100

    style: layout of income page

--- personal_finance_tracker/finance_tracker/forms.py
+++ personal_finance_tracker/finance_tracker/forms.py
@@ -13,8 +13,17 @@ class IncomeForm(forms.ModelForm):
     class Meta:
         model = Income
         fields = ['amount', 'source', 'date_received', 'time_received', 'describtion']
+        labels = {
+            'amount': '',
+            'source': '',
+            'date_received': '',
+            'time_received': '',
+            'describtion': '',
+        }
         widgets = {
-            'date_received': forms.DateInput(attrs={'type': 'date'}),
-            'time_received': forms.TimeInput(attrs={'type': 'time'}),
-            'describtion': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control', 'id': 'amount-field', 'placeholder':'Amount'}),
+            'source': forms.TextInput(attrs={'class': 'form-control', 'id': 'source-field', 'placeholder': 'Source'}),
+            'date_received': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'time_received': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'describtion': forms.Textarea(attrs={'class': 'form-control', 'id': 'description-field', 'placeholder': 'Add a brief description (optional)', 'rows': 4}),
         }
